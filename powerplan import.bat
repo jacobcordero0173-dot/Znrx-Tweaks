be0de3e23811252f00569ab5fb7612c9c28a98fe
jacobcordero0173-dot <jacobcordero0173@gmail.com>
2026-05-02 05:11:59 -0700

    Update URL for downloading power plan file

--- powerplan import.bat
+++ powerplan import.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Downloading power plan...
 
-curl -L -o ZnrxPlan.pow "https://raw.githubusercontent.com/jacobcordero0173-dot/Znrx-Tweaks/main/Znrx%20Ultimate%20Laptop%20PowerPlan.pow"
+curl -L -o ZnrxPlan.pow "https://raw.githubusercontent.com/jacobcordero0173-dot/Znrx-Tweaks/refs/heads/main/Znrx%20Ultimate%20Laptop%20PowerPlan.pow"
 
 powercfg -import Znrx Ultimate Laptop PowerPlan.pow
 
