3ddfa437a01b96bbd59d54d8f6b2504bb5a59607
jacobcordero0173-dot <jacobcordero0173@gmail.com>
2026-05-02 05:06:58 -0700

    Fix import command for power plan file

--- powerplan import.bat
+++ powerplan import.bat
@@ -3,7 +3,7 @@ echo Downloading power plan...
 
 curl -L -o ZnrxPlan.pow "https://raw.githubusercontent.com/jacobcordero0173-dot/Znrx-Tweaks/main/Znrx%20Ultimate%20Laptop%20PowerPlan.xml"
 
-powercfg -import Znrx Ultimate Laptop PowerPlan.pow
+powercfg -import Znrx Ultimate Laptop PowerPlan.xml
 
 echo Installed!
 pause
