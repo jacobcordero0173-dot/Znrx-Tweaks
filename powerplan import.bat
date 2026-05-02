56df3b27f59f20402168461bfcafd71aa10addfc
jacobcordero0173-dot <jacobcordero0173@gmail.com>
2026-05-02 05:10:33 -0700

    Update power plan import script to use .pow file

--- powerplan import.bat
+++ powerplan import.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Downloading power plan...
 
-curl -L -o ZnrxPlan.pow "https://raw.githubusercontent.com/jacobcordero0173-dot/Znrx-Tweaks/main/Znrx%20Ultimate%20Laptop%20PowerPlan.xml"
+curl -L -o ZnrxPlan.pow "https://raw.githubusercontent.com/jacobcordero0173-dot/Znrx-Tweaks/main/Znrx%20Ultimate%20Laptop%20PowerPlan.pow"
 
-powercfg -import Znrx Ultimate Laptop PowerPlan.xml
+powercfg -import Znrx Ultimate Laptop PowerPlan.pow
 
 echo Installed!
 pause
